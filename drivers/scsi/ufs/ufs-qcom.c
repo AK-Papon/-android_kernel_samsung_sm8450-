ce0580b9574c5f64c6f5c3ac41f312f6f04556fa
HELLBOY017 <abhaygill017@gmail.com>
2023-10-23 01:58:44 +0530

    treewide: Fix LLVM 18 warns

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -4890,6 +4890,218 @@ static ssize_t irq_affinity_support_show(struct device *dev,
 
 static DEVICE_ATTR_RW(irq_affinity_support);
 
+/* for manual gc */
+static ssize_t manual_gc_show(struct device *dev,
+			struct device_attribute *attr, char *buf)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+	struct ufs_qcom_host *host = ufshcd_get_variant(hba);
+
+	u32 status = MANUAL_GC_OFF;
+
+	if (host->manual_gc.state == MANUAL_GC_DISABLE)
+		return scnprintf(buf, PAGE_SIZE, "%s", "disabled\n");
+
+	if (host->manual_gc.hagc_support) {
+		int err;
+
+		if (!ufshcd_hagc_eh_in_progress(hba)) {
+			pm_runtime_get_sync(hba->dev);
+			err = ufshcd_query_attr_retry(hba,
+				UPIU_QUERY_OPCODE_READ_ATTR,
+				QUERY_ATTR_IDN_MANUAL_GC_STATUS, 0, 0, &status);
+			pm_runtime_put_sync(hba->dev);
+			host->manual_gc.hagc_support = err ? false: true;
+		}
+	}
+
+	if (!host->manual_gc.hagc_support)
+		return scnprintf(buf, PAGE_SIZE, "%s", "bkops\n");
+
+	dev_err(dev, "%s status=%d\n", __func__, status);
+	return scnprintf(buf, PAGE_SIZE, "%s\n",
+			status == MANUAL_GC_STATUS_CLEAN ? "GREEN" :
+			status == MANUAL_GC_STATUS_PAUSE ? "YELLOW" :
+			status == MANUAL_GC_STATUS_DIRTY ? "RED" :
+			status == MANUAL_GC_STATUS_MAX ? "UNKNOWN" : "UNKNOWN");
+}
+
+static int manual_gc_enable(struct ufs_hba *hba, u32 *value)
+{
+	int ret;
+
+	if (ufshcd_hagc_eh_in_progress(hba))
+		return -EBUSY;
+
+	dev_err(hba->dev, "%s value=%d\n", __func__, *value);
+	pm_runtime_get_sync(hba->dev);
+	ret = ufshcd_query_attr_retry(hba,
+				UPIU_QUERY_OPCODE_WRITE_ATTR,
+				QUERY_ATTR_IDN_MANUAL_GC_CONT, 0, 0,
+				value);
+	pm_runtime_put_sync(hba->dev);
+	dev_err(hba->dev, "%s ret=%d\n", __func__, ret);
+	return ret;
+}
+
+static ssize_t manual_gc_store(struct device *dev,
+			struct device_attribute *attr,
+			const char *buf, size_t count)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+	struct ufs_qcom_host *host = ufshcd_get_variant(hba);
+
+	u32 value;
+	int err = 0;
+
+	if (kstrtou32(buf, 0, &value))
+		return -EINVAL;
+
+	if (value >= MANUAL_GC_MAX)
+		return -EINVAL;
+
+	if (ufshcd_hagc_eh_in_progress(hba))
+		return -EBUSY;
+
+	if (value == MANUAL_GC_DISABLE || value == MANUAL_GC_ENABLE) {
+		host->manual_gc.state = value;
+		return count;
+	}
+	if (host->manual_gc.state == MANUAL_GC_DISABLE)
+		return count;
+
+	if (host->manual_gc.hagc_support)
+		host->manual_gc.hagc_support =
+			manual_gc_enable(hba, &value) ? false : true;
+
+	pm_runtime_get_sync(hba->dev);
+
+	if (!host->manual_gc.hagc_support) {
+		err = ufshcd_bkops_ctrl(hba, (value == MANUAL_GC_ON) ?
+					BKOPS_STATUS_NON_CRITICAL:
+					BKOPS_STATUS_CRITICAL);
+		if (!hba->auto_bkops_enabled)
+			err = -EAGAIN;
+	}
+
+	/* flush wb buffer */
+	if (hba->dev_info.wspecversion >= 0x0310) {
+		enum query_opcode opcode = (value == MANUAL_GC_ON) ?
+						UPIU_QUERY_OPCODE_SET_FLAG:
+						UPIU_QUERY_OPCODE_CLEAR_FLAG;
+		u8 index = ufshcd_wb_get_query_index(hba);
+
+		ufshcd_query_flag_retry(hba, opcode,
+				QUERY_FLAG_IDN_WB_BUFF_FLUSH_DURING_HIBERN8,
+				index, NULL);
+		ufshcd_query_flag_retry(hba, opcode,
+				QUERY_FLAG_IDN_WB_BUFF_FLUSH_EN, index, NULL);
+	}
+
+	if (err || hrtimer_active(&host->manual_gc.hrtimer)) {
+		pm_runtime_put_sync(hba->dev);
+		return count;
+	} else {
+		/* pm_runtime_put_sync in delay_ms */
+		hrtimer_start(&host->manual_gc.hrtimer,
+			ms_to_ktime(host->manual_gc.delay_ms),
+			HRTIMER_MODE_REL);
+	}
+	return count;
+}
+
+static DEVICE_ATTR_RW(manual_gc);
+
+static ssize_t manual_gc_hold_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+	struct ufs_qcom_host *host = ufshcd_get_variant(hba);
+
+	return snprintf(buf, PAGE_SIZE, "%lu\n", host->manual_gc.delay_ms);
+}
+
+static ssize_t manual_gc_hold_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+	struct ufs_qcom_host *host = ufshcd_get_variant(hba);
+
+	unsigned long value;
+
+	if (kstrtoul(buf, 0, &value))
+		return -EINVAL;
+
+	if (value < UFSHCD_MANUAL_GC_HOLD_HIBERN8_MIN ||
+		value > UFSHCD_MANUAL_GC_HOLD_HIBERN8_MAX) {
+		dev_err(dev, "%s Invaild value: %d\n", __func__, value);
+		return -EINVAL;
+	}
+	host->manual_gc.delay_ms = value;
+	return count;
+}
+
+static DEVICE_ATTR_RW(manual_gc_hold);
+
+static enum hrtimer_restart mgc_hrtimer_handler(struct hrtimer *timer)
+{
+	struct ufs_qcom_host *host = container_of(timer, struct ufs_qcom_host,
+					manual_gc.hrtimer);
+
+	queue_work(host->manual_gc.mgc_workq, &host->manual_gc.hibern8_work);
+	return HRTIMER_NORESTART;
+}
+
+static void mgc_hibern8_work(struct work_struct *work)
+{
+	struct ufs_qcom_host *host = container_of(work, struct ufs_qcom_host,
+					manual_gc.hibern8_work);
+	pm_runtime_put_sync(host->hba->dev);
+	/* bkops will be disabled when power down */
+}
+
+static struct attribute *ufs_nt_sysfs_attrs[] = {
+	&dev_attr_manual_gc.attr,
+	&dev_attr_manual_gc_hold.attr,
+	NULL
+};
+
+static const struct attribute_group ufs_nt_sysfs_group = {
+	.name = "nt",
+	.attrs = ufs_nt_sysfs_attrs,
+};
+
+static int ufs_nt_init_sysfs(struct ufs_hba *hba)
+{
+	int ret;
+
+	ret = sysfs_create_group(&hba->dev->kobj, &ufs_nt_sysfs_group);
+	if (ret)
+		dev_err(hba->dev, "%s: Failed to create manual_gc sysfs group (err = %d)\n",
+				__func__, ret);
+
+	return ret;
+}
+
+void init_manual_gc(struct ufs_hba *hba)
+{
+	struct ufs_qcom_host *host = ufshcd_get_variant(hba);
+	struct ufs_manual_gc *mgc = &host->manual_gc;
+	char wq_name[23];
+
+	mgc->state = MANUAL_GC_ENABLE;
+	mgc->hagc_support = true;
+	mgc->delay_ms = UFSHCD_MANUAL_GC_HOLD_HIBERN8;
+
+	hrtimer_init(&mgc->hrtimer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	mgc->hrtimer.function = mgc_hrtimer_handler;
+
+	INIT_WORK(&mgc->hibern8_work, mgc_hibern8_work);
+	snprintf(wq_name, ARRAY_SIZE(wq_name), "ufs_mgc_hibern8_work_%d",
+			hba->host->host_no);
+	host->manual_gc.mgc_workq = create_singlethread_workqueue(wq_name);
+}
+
 static struct attribute *ufs_qcom_sysfs_attrs[] = {
 	&dev_attr_err_state.attr,
 	&dev_attr_power_mode.attr,
